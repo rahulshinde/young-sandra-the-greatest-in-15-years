c8298928d36b5d44f9477d0bf66953800fc50197
Rahul Shinde <rshinde@mica.edu>
2016-12-20 23:57:53 -0500

    adding in background

---
+++ js/main.js
@@ -0,0 +1,65 @@
+Site = {}
+
+$(document).ready( function(){
+  // root size
+  Site.root_size = $(window).width() / 76.8;
+  $('html').css({ 'font-size': Site.root_size });
+  $(window).resize(changeRootFontSize);
+
+  $(document).on('scroll', Site.backgroundChange);
+})
+
+Site.backgroundChange = function(){
+  Site.scroll = $(document).scrollTop() + $(window).height();
+  Site.section1 = $('#section1').offset().top + ($(window).height() * 0.5);
+  Site.section2 = $('#section2').offset().top + ($(window).height() * 0.5);
+  Site.section3 = $('#section3').offset().top + ($(window).height() * 0.5);
+  Site.section4 = $('#section4').offset().top + ($(window).height() * 0.5);
+  Site.section5 = $('#section5').offset().top + ($(window).height() * 0.5);
+  Site.section6 = $('#section6').offset().top + ($(window).height() * 0.5);
+
+  if (Site.scroll >= Site.section1 && Site.scroll < Site.section2 ){
+    if($('#section1_background').hasClass('show')){
+      return
+    }
+    $('.background').removeClass('show');
+    $('#section1_background').addClass('show');
+  }else if (Site.scroll >= Site.section2 && Site.scroll < Site.section3 ){
+    if($('#section2_background').hasClass('show')){
+      return
+    }
+    $('.background').removeClass('show');
+    $('#section2_background').addClass('show');
+  }else if (Site.scroll >= Site.section3 && Site.scroll < Site.section4 ){
+    if($('#section3_background').hasClass('show')){
+      return
+    }
+    $('.background').removeClass('show');
+    $('#section3_background').addClass('show');
+  }else if (Site.scroll >= Site.section4 && Site.scroll < Site.section5){
+    if($('#section4_background').hasClass('show')){
+      return
+    }
+    $('.background').removeClass('show');
+    $('#section4_background').addClass('show');
+  }else if (Site.scroll >= Site.section5 && Site.scroll < Site.section6){
+    if($('#section5_background').hasClass('show')){
+      return
+    }
+    $('.background').removeClass('show');
+    $('#section5_background').addClass('show');
+  }else if (Site.scroll >= Site.section6){
+    if($('#section6_background').hasClass('show')){
+      return
+    }
+    $('.background').removeClass('show');
+    $('#section6_background').addClass('show');
+  } else{
+    $('.background').removeClass('show');
+  }
+}
+
+changeRootFontSize = function() {
+  root_size = $(window).width() / 76.8;
+  $('html').css({ 'font-size': root_size });
+}
